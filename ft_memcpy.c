5b486e8f14c01c5437bbade28c8441291a07bbb9
Chems Hoflack <72741076+StudioBereus@users.noreply.github.com>
2023-10-19 17:27:10 +0200

    Add files via upload

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -5,21 +5,23 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: event <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/19 15:09:00 by event             #+#    #+#             */
-/*   Updated: 2023/10/19 16:32:42 by event            ###   ########.fr       */
+/*   Created: 2023/10/19 17:12:16 by event             #+#    #+#             */
+/*   Updated: 2023/10/19 17:25:56 by event            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<stddef.h>
+/*#include<stdio.h>
+#include<stdlib.h>*/
 
 void	*ft_memcpy(void *dst, const void *src, size_t sze)
 {
-	size_t	i;
-	char	*dst2;
+	size_t		i;
+	char		*dst2;
 	const char	*src2;
 
 	dst2 = dst;
-	src2 = src;  
+	src2 = src;
 	i = 0;
 	while (i < sze)
 	{
@@ -29,11 +31,12 @@ void	*ft_memcpy(void *dst, const void *src, size_t sze)
 	return (dst2);
 }
 
-int	main(void)
+/*int	main(void)
 {
 	const void *src = "abc";
-	void *dst = "";
+	void *dst = malloc(3);
+	char *rdest = (char *) dst;
 	
 	ft_memcpy(dst, src, 3);
-	printf("%s", dst);
-}
+	printf("%s", rdest);
+}*/
