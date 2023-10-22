820c6010d45f08808270ca564e53bebba1e2ac91
Chems Hoflack <72741076+StudioBereus@users.noreply.github.com>
2023-10-22 15:23:56 +0200

    Add files via upload

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,43 +1,48 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   strnstr.c                                          :+:      :+:    :+:   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: chhoflac <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/21 09:18:23 by chhoflac          #+#    #+#             */
-/*   Updated: 2023/10/21 11:52:04 by chhoflac         ###   ########.fr       */
+/*   Updated: 2023/10/22 15:22:29 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-	
+
+#include<stdio.h>
+#include<string.h>
+
 char	*ft_strnstr(const char *big, const char *lit, size_t len)
 {
 	size_t	i;
 	size_t	cnt;
- 	size_t	j;
+	size_t	j;
 
 	i = 0;
 	cnt = 0;
-	j = 0;
-	while ((i < len) && (big[i]))
+	if (lit[0] == '\0' || !lit)
+		return ((char *)big);
+	while (lit[i])
 	{
-		while ((big[j + cnt] == lit[j + cnt]) && (cnt < len))
+		j = i;
+		while ((big[i + cnt] == lit[0 + cnt]))
 		{
 			cnt++;
 		}
 		i++;
 	}
 	if (cnt == len)
-		return (big[j - cnt])
+		return ((char *)&big[i - cnt]);
 	else
-		return(null);
+		return(NULL);
 }
 
 int main(void)
 {
-	char *str1 = "abcdef";
+	char *str1 = "abcdefcde";
 	char *str2 = "cde";
 	
-	printf("%p %p", ft_strnstr(str1, str2, 3), strnstr(str1, str2, 3));
+	printf("%s", ft_strnstr(str1, str2, 7));
+	//printf("%s", strnstr(str1, str2, 3));
 }
-
