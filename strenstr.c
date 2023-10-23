416a3abfa7b74c3def3fcebc7c5f00930b7d3fb0
Chems Hoflack <72741076+StudioBereus@users.noreply.github.com>
2023-10-23 18:03:23 +0200

    Add files via upload

--- strenstr.c
+++ strenstr.c
@@ -6,28 +6,41 @@
 /*   By: event <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 16:16:55 by event             #+#    #+#             */
-/*   Updated: 2023/10/23 17:07:29 by event            ###   ########.fr       */
+/*   Updated: 2023/10/23 18:02:57 by event            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include<stddef.h>
+#include<string.h>
+#include<stdio.h>
 char	*ft_strnstr(const char *big, const char *lit, size_t len)
 {
 	size_t	i;
 	size_t	j;
-	
+
 	i = 0;
 	j = 0;
 	if (lit[0] == '\0')
-		return (big);
-	while (i < len && big[i])
+		return ((char *)big);
+	while (big[i] && i < len)
 	{
 		if (big[i] == lit[0])
 		{
-			while ((big[i + j] == lit[j]) && (i + j < len))
-			{
+			while (lit[j] && (big[i + j] == lit[j]) && (i + j < len))
 				j++;
-			}
 		}
 		i++;
 	}
+	if (j == strlen(lit) - 1)
+		return ((char *)big[len - j]);
+	else
+		return (NULL);
 }
+
+int main(void)
+{
+	char *str1 = "abcnecdef";
+	char *str2 = "cde";
+	
+	printf("%s", ft_strnstr(str1, str2, 9));
+}	
