55e4b71bec99ab8c197eed7c87be490a58121e79
Chems Hoflack <chhoflac@k2r2p3.42mulhouse.fr>
2023-10-27 17:49:05 +0200

    FT_STRLCPY

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: chhoflac <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 08:59:25 by chhoflac          #+#    #+#             */
-/*   Updated: 2023/10/27 16:00:38 by chhoflac         ###   ########.fr       */
+/*   Updated: 2023/10/27 17:38:08 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
-
+	
 	i = 0;
-	while ((src[i]) && (i < size - 1))
+	while ((src[i]) && (i < size - 1) && dst[i])
 	{	
 		dst[i] = src[i];
 		i++;		
@@ -30,7 +30,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 int main(void)
 {
 	const char *str1 = "abc";
-	char str2[] = "";
+	char *str2 = NULL;
 	size_t i;
 	i = ft_strlcpy(str2, str1, 4);
 	printf("%ld %s", i, str2);
