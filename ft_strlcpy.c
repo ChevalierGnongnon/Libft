5f95c161fb745c973ad1cb791aab4e97d5ae1aef
Chems Hoflack <72741076+StudioBereus@users.noreply.github.com>
2023-10-27 16:01:57 +0200

    Add files via upload

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: chhoflac <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 08:59:25 by chhoflac          #+#    #+#             */
-/*   Updated: 2023/10/27 15:35:32 by chhoflac         ###   ########.fr       */
+/*   Updated: 2023/10/27 16:00:38 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,20 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	size_t	i;
 
 	i = 0;
-	size += 1;
 	while ((src[i]) && (i < size - 1))
 	{	
 		dst[i] = src[i];
 		i++;		
 	}
-	dst[size + 1] = '\0';
+	dst[i] = '\0';
 	return(strlen(src)); 
 }
 
 int main(void)
 {
 	const char *str1 = "abc";
-	char *str2 = " ";
+	char str2[] = "";
 	size_t i;
 	i = ft_strlcpy(str2, str1, 4);
-	printf("%ld  aaaa", i);
+	printf("%ld %s", i, str2);
 }
