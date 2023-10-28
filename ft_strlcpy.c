fa497c7b7250f10408df53f829c339d073611947
Chems Hoflack <chhoflac@k1r3p8.42mulhouse.fr>
2023-10-28 08:22:54 +0200

    correction de strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,37 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   strlcpy.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: chhoflac <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/27 08:59:25 by chhoflac          #+#    #+#             */
-/*   Updated: 2023/10/27 17:38:08 by chhoflac         ###   ########.fr       */
+/*   Created: 2023/10/28 07:55:23 by chhoflac          #+#    #+#             */
+/*   Updated: 2023/10/28 08:20:59 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-#include <stdio.h>
+#include "libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+size_t	 ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
 	
 	i = 0;
-	while ((src[i]) && (i < size - 1) && dst[i])
-	{	
+	while ((src[i]) && i < (size - 1) && (size != 0))
+	{
 		dst[i] = src[i];
-		i++;		
+		i++;
 	}
-	dst[i] = '\0';
-	return(strlen(src)); 
-}
-
-int main(void)
-{
-	const char *str1 = "abc";
-	char *str2 = NULL;
-	size_t i;
-	i = ft_strlcpy(str2, str1, 4);
-	printf("%ld %s", i, str2);
+	if (i < size)
+		dst[i] = '\0';
+	return(strlen(src));
 }
