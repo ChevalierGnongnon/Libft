4e4728b4572b02edde3b453a6662ee2bebbd8b5b
Chems Hoflack <72741076+StudioBereus@users.noreply.github.com>
2023-10-28 13:49:38 +0200

    Add files via upload

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,19 +6,18 @@
 /*   By: chhoflac <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 08:10:46 by chhoflac          #+#    #+#             */
-/*   Updated: 2023/10/28 11:59:58 by chhoflac         ###   ########.fr       */
+/*   Updated: 2023/10/28 13:47:24 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<stddef.h>
-#include<string.h>
-#include<stdio.h>
+#include"libft.h"
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	char	*d2;
+	char		*d2;
 	const char	*s2;
-	size_t	i;
+	size_t		i;
 
 	d2 = dest;
 	s2 = src;
@@ -40,14 +39,4 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 		}
 	}
 	return (dest);
-}
-int main(void)
-{
-	char dest[5];
-	const char src[5]= {'1','2', '3','4', '5'};
-	
-	//ft_memmove(dest, src, 5);
-	memmove (dest, src, 5);
-	printf("%s", dest);
-	
 }
