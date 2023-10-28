b5f7c1572687f173bd4c893ccd4fd8c12c0a865e
Chems Hoflack <chhoflac@k1r3p8.42mulhouse.fr>
2023-10-28 08:55:39 +0200

    ajout de size verif

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,28 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   strlcpy.c                                          :+:      :+:    :+:   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: chhoflac <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 07:55:23 by chhoflac          #+#    #+#             */
-/*   Updated: 2023/10/28 08:20:59 by chhoflac         ###   ########.fr       */
+/*   Updated: 2023/10/28 08:54:02 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include"libft.h"
 
 size_t	 ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
 	
 	i = 0;
-	while ((src[i]) && i < (size - 1) && (size != 0))
+	
+	if (size == 0)
+		return (strlen(src));
+	while ((src[i]) && i < (size - 1))
 	{
 		dst[i] = src[i];
 		i++;
 	}
 	if (i < size)
 		dst[i] = '\0';
-	return(ft_strlen(src));
+	return(strlen(src));
 }
