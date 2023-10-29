275fcb359ba18031f8398edf2a6efc2ec82fde51
Chems Hoflack <72741076+StudioBereus@users.noreply.github.com>
2023-10-29 11:43:47 +0100

    Add files via upload

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: chhoflac <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/29 07:05:51 by chhoflac          #+#    #+#             */
-/*   Updated: 2023/10/29 09:29:19 by chhoflac         ###   ########.fr       */
+/*   Updated: 2023/10/29 11:41:57 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,14 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 
 	i = strlen(dst);
 	j = 0;
-	while ((src[j] != '\0') && (j < size - 1))
+	if (size == 0 || size <= strlen(dst)) 
+		return (size + strlen(src));
+	while ((src[j] != '\0') && ((j + i) < size - 1))
 	{
 		dst[i + j] = src[j];
 		j++; 
 	}
 	if (j < size)
-		dst[j + i] = '\0';
-	return (i + j + 1);
+		dst[i + j] = '\0';
+	return (i + j);
 }
-/*int main(void)
-{
-	char str1[7] = {'a', 'b', 'c'};
-	const char *str2 = "def";
-	size_t i = ft_strlcat(str1, str2, 7);
-	printf("%s, %ld", str1, i);
-}*/
