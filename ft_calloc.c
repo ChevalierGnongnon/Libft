f97926747686dc27ce27de0fa87aa463d18c373f
Chems Hoflack <chhoflac@k1r3p8.42mulhouse.fr>
2023-10-30 10:29:08 +0100

    essai

---
+++ ft_calloc.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chhoflac <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/30 08:11:12 by chhoflac          #+#    #+#             */
+/*   Updated: 2023/10/30 10:15:13 by chhoflac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<stddef.h>
+#include<stdlib.h>
+#include<strings.h>
+#include<stdio.h>
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*ptr;
+	
+	ptr = (void *) malloc(nmemb * size);
+	if (ptr == NULL)
+                return (NULL);
+	bzero(ptr, nmemb * size);
+	printf("a");
+	return (ptr);
+}
+int main(void)
+{
+	void *ptr;
+	ptr = ft_calloc(12345678, 456);
+	printf("%p", ptr);
+}
