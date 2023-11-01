4428a26be4d1d7ff89cfcf2ea1acf60a7287a9f1
Chems Hoflack <chhoflac@k1r3p8.42mulhouse.fr>
2023-11-01 16:08:21 +0100

    modif substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: chhoflac <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 12:02:30 by chhoflac          #+#    #+#             */
-/*   Updated: 2023/11/01 15:12:50 by chhoflac         ###   ########.fr       */
+/*   Updated: 2023/11/01 15:59:21 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,20 +22,22 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	i = 0;
         sub = (char *) malloc(len + 1);
-	if(!sub || !s)
+	if(!sub)
 		return (NULL);
-	while(i < len && s[i])
+	if(strlen(s) > start)
 	{
-		sub[i] = s[start + i];
-		i++;
+		while(i < len && s[i])
+		{
+			sub[i] = s[start + i];
+			i++;
+		}
 	}
 	sub[i] = '\0';
 	return (sub);
 }
 int main(void)
 {
-	char	*str1 = "abcsw";
-	char	*sub = ft_substr(str1, 0, 3);
+	char	*str1 = "abcdef";
+	char	*sub = ft_substr(str1, INT_MAX, 12);
 	printf("%s", sub);
 }
-
