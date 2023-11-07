daf71fc10be482ec992d235ba9714f8f48c3024b
Chems Hoflack <chhoflac@k2r3p9.42mulhouse.fr>
2023-11-07 10:48:23 +0100

    avancee sur itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,12 +6,15 @@
 /*   By: chhoflac <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 13:05:44 by chhoflac          #+#    #+#             */
-/*   Updated: 2023/11/07 08:35:42 by chhoflac         ###   ########.fr       */
+/*   Updated: 2023/11/07 10:39:31 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
 static int	ft_getsize(int n)
 {
 	size_t	j;
@@ -24,6 +27,7 @@ static int	ft_getsize(int n)
 	}
 	return (j); 
 }
+
 static char	*ft_alloc(int n)
 {
 	char	*s;
@@ -35,15 +39,34 @@ static char	*ft_alloc(int n)
 	return (s);
 }
 
-char *ft_itoa(int n)
+char	*ft_itoa(int n)
 {
 	char	*str;
-	int	size;
+	int	i;
 	
-	s = ft_alloc(n);
+	str = ft_alloc(n);
+	i = 0;
+	if (!str)
+		return (NULL);
+	if(n < 0)
+	{
+		n *= -1;
+		str[0] = '-';
+		i++;
+	}
+	i = ft_getsize(n); 
+	while(n != 0)
+	{
+		str[i - 1] = n + 48;
+		n /= 10;
+		i--;
+	}
+	return (str);		
 }
 
 int main(void)
 {
-
+	int a = 123;
+	char *str = ft_itoa(a);
+	printf("%s", str);
 }
