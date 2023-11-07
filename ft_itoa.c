032c221719db3e5b80009cde7193ded9ed19e269
Chems Hoflack <chhoflac@k2r3p9.42mulhouse.fr>
2023-11-07 08:36:40 +0100

    avancee sur itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,13 +6,13 @@
 /*   By: chhoflac <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 13:05:44 by chhoflac          #+#    #+#             */
-/*   Updated: 2023/11/06 13:52:20 by chhoflac         ###   ########.fr       */
+/*   Updated: 2023/11/07 08:35:42 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 #include <stdio.h>
-static int	ft_definesize(int n)
+static int	ft_getsize(int n)
 {
 	size_t	j;
 	
@@ -24,13 +24,23 @@ static int	ft_definesize(int n)
 	}
 	return (j); 
 }
+static char	*ft_alloc(int n)
+{
+	char	*s;
+
+	if(n >= 0)
+		s = (char *) malloc(ft_getsize(n) + 1);
+	else
+		s = (char *) malloc(ft_getsize(n) + 2);
+	return (s);
+}
 
 char *ft_itoa(int n)
 {
-	char *str;
+	char	*str;
+	int	size;
 	
-	str = (char *) malloc(ft_definesize(n));
-	
+	s = ft_alloc(n);
 }
 
 int main(void)
