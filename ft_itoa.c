944c78bc6ce501ddba815f9c14b187535ef1b52a
Chems Hoflack <chhoflac@k1r3p8.42mulhouse.fr>
2023-11-08 11:53:29 +0100

    ne fonctionne qu avec des negsatifs

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: chhoflac <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 13:05:44 by chhoflac          #+#    #+#             */
-/*   Updated: 2023/11/07 15:20:25 by chhoflac         ###   ########.fr       */
+/*   Updated: 2023/11/08 11:52:26 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+#include "ft_calloc.c"
 
 static int	ft_getsize(int n)
 {
@@ -33,9 +34,9 @@ static char	*ft_alloc(int n)
 	char	*s;
 
 	if(n >= 0)
-		s = (char *) malloc(ft_getsize(n) + 1);
+		s = (char *) ft_calloc(ft_getsize(n) + 1, 1);
 	else
-		s = (char *) malloc(ft_getsize(n) + 2);
+		s = (char *) ft_calloc(ft_getsize(n) + 2, 1);
 	return (s);
 }
 
@@ -43,28 +44,25 @@ char	*ft_itoa(int n)
 {
 	char	*str;
 	int	i;
-//	int	mult;
+	int	stock;
 	
+	stock = ft_getsize(n) + 1;
 	str = ft_alloc(n);
-	i = 0;
-//	mult = 10;
+	i = ft_getsize(n);
 	if (!str)
 		return (NULL);
-	if(n < 0)
+	if (n < 0)
 	{
 		n *= -1;
 		str[0] = '-';
 	}
-	i = ft_getsize(n + 1);
-	printf("before loop : i : %d n : %d\n", i, n); 
-	while(i > 0 && n > 0)
+	while((i >= 0 || str[i] == '-') && n > 0)
 	{
-		printf("i : %d n : %d \n", i, n);
 		str[i] = (n % 10) + 48;
 		n /= 10;
 		i--;
 	}
-	str[ft_getsize(n) + 1] = '\0';
+	str[stock] = '\0';
 	return (str);
 }
 
@@ -73,4 +71,5 @@ int main(void)
 	int a = -123;
 	char *str = ft_itoa(a);
 	printf("%s", str);
+	free(str);
 }
