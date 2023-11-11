a52167ff1e5ef376941832266f3bae8e555da56e
Chems Hoflack <chhoflac@k1r3p8.42mulhouse.fr>
2023-11-11 17:30:29 +0100

    a corriger

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,11 +6,12 @@
 /*   By: chhoflac <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 08:10:46 by chhoflac          #+#    #+#             */
-/*   Updated: 2023/11/11 14:07:46 by chhoflac         ###   ########.fr       */
+/*   Updated: 2023/11/11 17:17:25 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<stddef.h>
+#include <stdio.h>
 #include"libft.h"
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
@@ -18,26 +19,32 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	char		*d2;
 	const char	*s2;
 	size_t		i;
+	size_t		temp;
 
-	d2 = dest;
-	s2 = src;
+	temp = n;
+	d2 = (char *) dest;
+	s2 = (char *) src;
 	i = 0;
 	if (dest > src)
-	{
 		while (n > 0)
 		{
 			d2[n] = s2[n];
 			n--;
 		}
-	}
 	else
-	{
-		while (i < n)
+		while (i < temp)
 		{
 			d2[i] = s2[i];
 			i++;
 		}
-	}
-	dest = d2;
-	return (dest);
-}		
+	return (d2);
+}
+
+int main(void)
+{
+	char *dest = "   ";
+	const char *src = "abc";
+
+	printf("%s", (char *) ft_memmove(dest, src, 2));
+}
+
