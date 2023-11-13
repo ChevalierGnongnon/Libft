c8605cec4755a2d2a37a4958afca5e508d7e3d39
Chems Hoflack <chhoflac@k1r3p8.42mulhouse.fr>
2023-11-13 17:59:37 +0100

    corrections francinette et paco s ok

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: event <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 09:22:38 by event             #+#    #+#             */
-/*   Updated: 2023/11/02 15:24:49 by chhoflac         ###   ########.fr       */
+/*   Updated: 2023/11/13 17:42:26 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	size_t		i;
-	const char	*str;
+	unsigned char	*str;
 
 	i = 0;
-	str = s;
+	str = (unsigned char *)s;
 	while (i < n)
 	{
-		if (str[i] == c)
-			return ((char *) &str[i]);
+		if (str[i] == (unsigned char) c)
+			return ((void *) &str[i]);
 		else
 			i++;
 	}
