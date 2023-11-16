a534e2ee161516c92cf6349847c3d04cb4858ea0
Chems Hoflack <chhoflac@k1r3p8.42mulhouse.fr>
2023-11-16 13:45:59 +0100

    ajout des scripts des fonctions bonus

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: chhoflac <chhoflac@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 15:38:22 by chhoflac          #+#    #+#             */
-/*   Updated: 2023/11/15 08:43:21 by chhoflac         ###   ########.fr       */
+/*   Updated: 2023/11/16 13:19:59 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,5 +54,14 @@ void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_bzero(void *s, size_t n);
 size_t	ft_strlen(const char *str);
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
