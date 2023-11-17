0dd1380b5d59c3ce3fdbf4a6a5d872c4a3809a41
Chems Hoflack <chhoflac@k1r3p8.42mulhouse.fr>
2023-11-17 13:26:47 +0100

    ajout lstsize fok lstlast fko

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: chhoflac <chhoflac@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 15:38:22 by chhoflac          #+#    #+#             */
-/*   Updated: 2023/11/17 12:38:09 by chhoflac         ###   ########.fr       */
+/*   Updated: 2023/11/17 13:02:17 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,8 +62,8 @@ void	ft_bzero(void *s, size_t n);
 size_t	ft_strlen(const char *str);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *n);
-/*int		ft_lstsize(t_list *lst);
-t_list	*ft_lstlast(t_list *lst);
+int		ft_lstsize(t_list *lst);
+/*t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
