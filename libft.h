b95946fb208c2c34deaa35b4210ae05dae3c9844
Chems Hoflack <chhoflac@k1r3p8.42mulhouse.fr>
2023-11-17 09:23:15 +0100

    ajout de lstnew + modif libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: chhoflac <chhoflac@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 15:38:22 by chhoflac          #+#    #+#             */
-/*   Updated: 2023/11/16 13:19:59 by chhoflac         ###   ########.fr       */
+/*   Updated: 2023/11/17 09:13:15 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,13 @@
 # include<stdio.h>
 # include <string.h>
 # include <limits.h>
+
+typedef struct	s_list
+{
+	void		*content;
+	struct s_list	*next;
+			t_list;
+}
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
@@ -55,13 +62,13 @@ void	ft_putnbr_fd(int n, int fd);
 void	ft_bzero(void *s, size_t n);
 size_t	ft_strlen(const char *str);
 t_list	*ft_lstnew(void *content);
-void	ft_lstadd_front(t_list **lst, t_list *new);
+/*void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));*/
 
 #endif
