a443558f3c1ef98c7cd0cf8d50815fd04ea643f3
Chems Hoflack <chhoflac@k1r3p8.42mulhouse.fr>
2024-04-06 14:37:39 +0200

    ajout de lstdelone

---
+++ ft_lstadd_back.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/06 13:59:14 by chhoflac          #+#    #+#             */
+/*   Updated: 2024/04/06 14:13:47 by chhoflac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last;
+
+	if (lst && *lst)
+	{
+		last = *lst;
+		while (last->next)
+			last = last->next;
+		last->next = new;
+	}
+	else if (lst)
+		*lst = new;
+}
